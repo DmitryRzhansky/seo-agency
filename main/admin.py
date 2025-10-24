197b3dff7afacc197f16cd6616a4612cf2a94dfb
rzh_exe <dima.rzhansky@gmail.com>
2025-10-24 08:29:35 +0500

    Admin refactor

--- main/admin.py
+++ main/admin.py
@@ -13,12 +13,6 @@ from django.contrib import admin
 # Регистрируем все модели в кастомном админ-сайте
 from .models import City, ServiceCategory, Service, TeamMember, Testimonial, ContactRequest, PortfolioItem, CustomHeadScript, RegionalPostAdaptation
 
+# Все модели уже зарегистрированы через декораторы @admin.register() в admin_seo.py
+# Регистрируем только City, так как у неё нет декоратора
 admin.site.register(City, CityAdmin)
-admin.site.register(ServiceCategory, ServiceCategoryAdmin)
-admin.site.register(Service, ServiceAdmin)
-admin.site.register(TeamMember, TeamMemberAdmin)
-admin.site.register(Testimonial, TestimonialAdmin)
-admin.site.register(ContactRequest, ContactRequestAdmin)
-admin.site.register(PortfolioItem, PortfolioItemAdmin)
-admin.site.register(CustomHeadScript, CustomHeadScriptAdmin)
-admin.site.register(RegionalPostAdaptation, RegionalPostAdaptationAdmin)

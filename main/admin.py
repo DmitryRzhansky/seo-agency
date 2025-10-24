cc168c38bbe32a47dc8ab91fb52793a1aa6b4f1f
rzh_exe <dima.rzhansky@gmail.com>
2025-10-24 08:28:10 +0500

    Admin

--- main/admin.py
+++ main/admin.py
@@ -13,12 +13,12 @@ from django.contrib import admin
 # Регистрируем все модели в кастомном админ-сайте
 from .models import City, ServiceCategory, Service, TeamMember, Testimonial, ContactRequest, PortfolioItem, CustomHeadScript, RegionalPostAdaptation
 
-#admin.site.register(City, CityAdmin)
-#admin.site.register(ServiceCategory, ServiceCategoryAdmin)
-#admin.site.register(Service, ServiceAdmin)
-#admin.site.register(TeamMember, TeamMemberAdmin)
-#admin.site.register(Testimonial, TestimonialAdmin)
-#admin.site.register(ContactRequest, ContactRequestAdmin)
-#admin.site.register(PortfolioItem, PortfolioItemAdmin)
-#admin.site.register(CustomHeadScript, CustomHeadScriptAdmin)
-#admin.site.register(RegionalPostAdaptation, RegionalPostAdaptationAdmin)
+admin.site.register(City, CityAdmin)
+admin.site.register(ServiceCategory, ServiceCategoryAdmin)
+admin.site.register(Service, ServiceAdmin)
+admin.site.register(TeamMember, TeamMemberAdmin)
+admin.site.register(Testimonial, TestimonialAdmin)
+admin.site.register(ContactRequest, ContactRequestAdmin)
+admin.site.register(PortfolioItem, PortfolioItemAdmin)
+admin.site.register(CustomHeadScript, CustomHeadScriptAdmin)
+admin.site.register(RegionalPostAdaptation, RegionalPostAdaptationAdmin)
